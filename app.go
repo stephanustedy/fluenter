c9a6e21816ce170b298ca2f263c23caffc743711
steph <stephanus.tedy@gmail.com>
2015-07-13 10:18:29 +0000

    initial commit

---
+++ app.go
@@ -0,0 +1,30 @@
+package fluenter
+
+import (
+	"net"
+	"log"
+)
+
+var tdConn *net.UDPConn
+
+func initUDP(host string){
+	if host == "" {
+		host = "ip-10-0-11-83.ap-southeast-1.compute.internal:7777"
+	}
+
+	//default AWS IP
+	fluentd, err := net.ResolveUDPAddr("udp", host)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    tdConn, err = net.DialUDP("udp", nil, fluentd)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func UdpToS3(tag string, jsonPacket string){
+	buf := []byte(packet)
+    tdConn.Write(buf)
+}
